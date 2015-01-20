6d1a443e8ec2f56861971f7c48d730877ff4840a
Igor <isem.com@gmail.com>
2015-01-20 22:48:21 +0200

    First full value commit from IDEA

--- src/com/kademika/day10/generics/wildcards/CarBox.java
+++ src/com/kademika/day10/generics/wildcards/CarBox.java
@@ -1,8 +1,22 @@
 package com.kademika.day10.generics.wildcards;
 
 import com.kademika.day10.domain.Car;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarBox<T extends Car> {
 
-    
+    private List<T> cars;
+
+    public CarBox() {
+        cars = new ArrayList<>();
+    }
+
+    public void addCar(T car) {
+        cars.add(car);
+    }
+
+    public List<T> getCars() {
+        return new ArrayList<>(cars);
+    }
 }
