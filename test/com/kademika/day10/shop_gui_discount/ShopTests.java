a4fb1f0e236ea701f6658ba415861b603416c590
Igor <isem.com@gmail.com>
2015-02-24 20:16:53 +0200

    ShopTests

--- test/com/kademika/day10/shop_gui_discount/ShopTests.java
+++ test/com/kademika/day10/shop_gui_discount/ShopTests.java
@@ -35,32 +35,53 @@ public class ShopTests {
 		shop.addToGarage(carInexpensive2);
 		shop.addToGarage(carExpensive2);
 		customer = new Customer("Tester", "Tester1", "+380001234567");
-	}
+    }
 
-	@Test
-	public void testSell() {
-		shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
-				,carInexpensive1, customer);
-		shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
-				,carExpensive1, customer);
-		shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
-				,carInexpensive2, customer);
-		shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
-				,carExpensive2, customer);
-		for (Transaction t : shop.getAllTransactions()) {
-			if (t.getCar().equals(carInexpensive1)) {
-				//discount 0% if price < 25k
-				assertEquals(24999.0, t.getCar().getPrice(), 0.0);
-			} else if (t.getCar().equals(carExpensive1)) {
-				//discount 5% (1250.0) if price >= 25k
-				assertEquals(23750.0, t.getCar().getPrice(), 0.0);
-			} else if (t.getCar().equals(carInexpensive2)) {
-				//discount 5% (1499.95) if price >= 25k
-				assertEquals(28499.05, t.getCar().getPrice(), 0.0);
-			} else if (t.getCar().equals(carExpensive2)) {
-				//discount 10% (3000.0) if price >= 30k
-				assertEquals(27000.0, t.getCar().getPrice(), 0.0);
-			}
-		}
-	}
+    @Test
+    //discount 0% if price < 25k
+    public void testSellCarInexpensive1() {
+        shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
+                , carInexpensive1, customer);
+        for (Transaction t : shop.getAllTransactions()) {
+            if (t.getCar().equals(carInexpensive1)) {
+                assertEquals(24999.0, t.getCar().getPrice(), 0.0);
+            }
+        }
+    }
+
+    @Test
+    //discount 5% (1250.0) if price >= 25k
+    public void testSellCarExpensive1() {
+        shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
+                , carExpensive1, customer);
+        for (Transaction t : shop.getAllTransactions()) {
+            if (t.getCar().equals(carExpensive1)) {
+                assertEquals(23750.0, t.getCar().getPrice(), 0.0);
+            }
+        }
+    }
+
+    @Test
+    //discount 5% (1499.95) if price >= 25k & < 30k
+    public void testSellCarInexpensive2() {
+        shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
+                , carInexpensive2, customer);
+        for (Transaction t : shop.getAllTransactions()) {
+            if (t.getCar().equals(carInexpensive2)) {
+                assertEquals(28499.05, t.getCar().getPrice(), 0.0);
+            }
+        }
+    }
+
+    @Test
+    //discount 10% (3000.0) if price >= 30k
+    public void testSellCarExpensive2() {
+        shop.sell(new SimpleDateFormat("dd-MM-yyyy kk:mm:ss").format(new Date())
+                , carExpensive2, customer);
+        for (Transaction t : shop.getAllTransactions()) {
+            if (t.getCar().equals(carExpensive2)) {
+                assertEquals(27000.0, t.getCar().getPrice(), 0.0);
+            }
+        }
+    }
 }
